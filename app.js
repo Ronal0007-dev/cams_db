86e57fec979b63483bb0edc385d912b2c9e6525c
Ronal0007-dev <kallagiga@gmail.com>
2026-01-30 20:17:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const createError = require('http-errors');
+const cookieSession = require('cookie-session');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+//setting cookie session
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+app.use(session({
+  name: 'session',
+  keys: ['s3cr3t_OnE', 's3cr3t_Tw0'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    domain: 'localhost',
+    path: 'foo/bar',
+    expires: expiryDate
+  }
+}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//disable fingerprinting
+app.disable('x-powered-by')
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//import db
+const db = require('../db_setup/db_setup');
+const bcrypt = require('bcrypt');
+
+router.use(express.urlencoded({ extended: true })); // Parse form data
+
+// GET home page
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
